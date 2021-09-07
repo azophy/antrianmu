317c6a54f34c41d0cc840590d8f184a8d374bcf0
Abdurrahman Shofy Adianto <azophy@gmail.com>
2021-09-07 22:03:55 +0700

    Add basic navbar & footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,84 @@
     <title>@yield('page_title', config('app.name'))</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
   </head>
-  <body>
-  <section class="section">
-    <div class="container">
-      @yield('content')
+<body>
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item title is-4" href="{{ url('/') }}">
+        {{ config('app.name') }}
+    </a>
+
+{{--
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+--}}
+  </div>
+{{--
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Documentation
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
     </div>
-  </section>
-  </body>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+--}}
+</nav>
+
+<section class="section">
+  <div class="container">
+    @yield('content')
+  </div>
+</section>
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>{{ config('app.name') }}</strong> by <a target="_blank" href="https://abdurrahman.adianto.id">Abdurrahman Shofy Adianto</a>. All right reserved.
+    </p>
+  </div>
+</footer>
+
+</body>
 </html>
