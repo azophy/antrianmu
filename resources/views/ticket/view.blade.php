be610051c1d7639752085d6ed95baae3ef59a6d2
Abdurrahman Shofy Adianto <azophy@gmail.com>
2021-09-07 23:47:08 +0700

    Improve ticket page design

--- resources/views/ticket/view.blade.php
+++ resources/views/ticket/view.blade.php
@@ -1,18 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="notification is-warning">
+  <strong>Mohon simpan halaman tiket ini</strong>
+  <p>Halaman ini adalah tanda bukti nomor antrian yang anda ambil. Silahkan simpan tautan/link halaman ini</p>
+</div>
 
-<h1 class="title">Tiket untuk antrian "{{ $queue->title }}"</h1>
+<div class="card mx-auto" style="max-width:400px">
+  <header class="card-header">
+    <p class="card-header-title">
+        Tiket untuk antrian "{{ $queue->title }}"
+    </p>
+    <button class="card-header-icon" aria-label="more options">
+      <span class="icon">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </header>
+  <div class="card-content">
+    <div class="content has-text-centered">
 
-<ul>
-  <li>Nomor antrian anda: <strong>{{ $ticket->order }}</strong></li>
-  <li>Nomor antrian saat ini: <strong>{{ $queue->ticket_current }}</strong></li>
-  <li>Nomor antrian terakhir: <strong>{{ $queue->ticket_last }}</strong></li>
-  <li>Tautan untuk tiket ini: <strong>
+      <div class="box is-primary">
+        <p>Nomor antrian anda:</p>
+        <p class="title" style="font-size:4rem">{{ $ticket->order }}</p>
+        <p>Kode tiket anda:</p>
+        <p class="title is-3 is-uppercase">{{ $ticket->secret_code }}</p>
+      </div>
+
+        <nav class="level is-mobile">
+          <div class="level-item">
+            <div>
+              <p class="heading">Antrian sekarang:</p>
+              <p class="title">{{ $queue->ticket_current }}</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">Antrian terakhir:</p>
+              <p class="title">{{ $queue->ticket_last }}</p>
+            </div>
+          </div>
+        </nav>
+
+        <br>
+        Perkiraan giliran: <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        <p>Tautan untuk tiket ini: </p>
         <a href="{{ route('ticket.view', ['code' => $ticket->secret_code ]) }}">
             {{ route('ticket.view', ['code' => $ticket->secret_code ]) }}
         </a>
-  </strong></li>
-</ul>
-
+    </div>
+  </div>
+</div>
 @endsection
