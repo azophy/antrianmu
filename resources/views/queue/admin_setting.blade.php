35630822941f3935178c241f5cef8a7ad8a2df4a
Abdurrahman Shofy Adianto <azophy@gmail.com>
2021-09-09 03:14:15 +0700

    Add package info notification on admin.setting

--- resources/views/queue/admin_setting.blade.php
+++ resources/views/queue/admin_setting.blade.php
@@ -34,6 +34,14 @@ $admin_setting_url = route('admin.setting', ['slug' => $queue->slug ]);
 </div>
 @endif
 
+@if ($queue->type == $queue::TYPE_FREE)
+<div class="notification is-info">
+    <p>
+        Antrian ini saat ini menggunakan paket "percobaan", yang artinya antrian ini hanya berlaku di tanggal ini ({{ $queue->created_at->format('d-m-Y') }}), dan dibatasi hanya sampai <strong>{{$queue->ticket_limit}} nomor</strong> saja. Silahkah upgrade paket antrian anda untuk mendapatkan lebih banyak fasilitas.
+    </p>
+</div>
+@endif
+
 <ul>
   <li>Kode rahasia untuk admin: <strong>{{ $queue->secret_code }}</strong></li>
   <li>Nomor antrian saat ini: <strong>{{ $queue->ticket_current }}</strong></li>
